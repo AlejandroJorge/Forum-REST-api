e5362646b0279ebd82c13643c2c40c1971ecda9a
AlejandroJorge <a20202060@pucp.edu.pe>
2024-01-28 21:31:23 -0500

    Server running

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+	"os"
+
+	"github.com/AlejandroJorge/forum-rest-api/config"
+	"github.com/AlejandroJorge/forum-rest-api/delivery/router"
+)
 
 func main() {
-	fmt.Println("Hello world")
+	config.Initialize()
+
+	fmt.Printf("Listening on http://localhost:%s", os.Getenv("PORT"))
+	http.ListenAndServe(fmt.Sprintf(":%s", os.Getenv("PORT")), router.AppRouter())
 }
